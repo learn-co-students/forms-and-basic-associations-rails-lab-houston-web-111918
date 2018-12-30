2033b8b7c5c459238fc4bd1168ccfecd49eac1a2
claire <github email address>
2018-12-29 23:10:16 -0800

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,7 +8,7 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    @song = Song.new 
   end
 
   def create
@@ -47,7 +47,8 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    # params.require(:song).permit(:title, :artist_name, :genre_id, note_contents: [])
+    params.require(:song).permit(:title, :artist_name, :genre_id, :note_contents => [])
   end
 end
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,42 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :genre
+  belongs_to :artist
+  has_many :notes
+
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+    
+  end
+
+  def artist_name
+     self.artist ? self.artist.name : nil
+    #  self.try(:artist).try(:name)
+    end
+
+  def genre_name=(name)
+    genre = Genre.find_or_create_by(name: name)
+    self.genre = genre
+    # self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    # byebug
+    self.genre ? self.genre.name : nil
+    # self.try(:genre).try(:name)
+  end
+
+  def note_contents=(notes)
+    notes.each do |content|
+      if content.strip != ''
+        self.notes.build(content: content)
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.map(&:content)
+  end
+  
 end
